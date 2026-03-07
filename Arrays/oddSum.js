4d3e6b372340248ca6e72ae69a7ec9c7a6055fe2
Deepak Pandey <deepakpandey101094@gmail.com>
2026-03-07 08:44:43 -0800

    DAY 5: find index of first no

--- Arrays/oddSum.js
+++ Arrays/oddSum.js
@@ -1,4 +1,5 @@
 // Return sum of all odd numbers.
+const arr = [10, 8, 9, 20, 7, 25, 15, 30, 28, 35, 35];
 
 function oddSum(arr) {
   let sum = 0
@@ -11,3 +12,16 @@ function oddSum(arr) {
 }
 const result = oddSum(arr);
 console.log(result);
+
+
+// Return index of first number divisible by both 2 and 5.
+function findIndex(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 10 === 0) {
+      return i
+    }
+  }
+  return -1;
+}
+
+console.log(findIndex(arr));
