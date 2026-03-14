08a110802edff2f457d51b4f35be1bd980c8a607
Deepak Pandey <deepakpandey101094@gmail.com>
2026-03-14 12:31:33 -0700

    DAY 9: find the misiing number

---
+++ Arrays/misingNumber.js
@@ -0,0 +1,17 @@
+/* 
+Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+*/
+
+function misingNumber(arr) {
+    let n = arr.length;
+    let totalSum = n * (n + 1) / 2;
+
+    let partialSum = 0;
+    for(let i = 0; i < n; i++){
+        partialSum = arr[i] + partialSum;
+    }
+    return totalSum - partialSum;
+}
+
+const result = misingNumber([4 , 0, 2, 1, 5 ]);
+console.log(result); 
