0aa78c43735a8fa1d1ac79a71a2dc5af5da0a6ba
Deepak Pandey <deepakpandey101094@gmail.com>
2026-03-18 21:58:12 -0700

    DSA 12: find the sum of all number from an array

---
+++ Recursion/sumOfAllNumber.js
@@ -0,0 +1,10 @@
+const arr = [ 5, 3, 2, 0 , 1];
+
+function sum(n) {
+    if(n == 0) return arr[n];
+
+    return arr[n] + sum(n - 1);
+};
+
+const result = sum(arr.length - 1);
+console.log(result);
